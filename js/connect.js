89d5d656b926a47b6f8db948c05560a2c3edc8b2
Jahidul002 <emon.me44@gmail.com>
2022-09-03 23:52:02 +0600

    blog site added

--- js/connect.js
+++ js/connect.js
@@ -3,6 +3,7 @@ const categoriList = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displaycategoriList(data.data.news_category))
+
 }
 const displaycategoriList = allList => {
     // console.log(allList);
@@ -12,7 +13,7 @@ const displaycategoriList = allList => {
         // console.log(theList);
         const li = document.createElement('li');
         li.innerHTML = `
-    <a onclick="loadCardData('${theList.category_id}')" class="text-decoration-none fw-semibold li-txt" href="#">${theList.category_name}</a>
+    <a onclick="loadData('${theList.category_id}')" class="text-decoration-none fw-semibold li-txt" href="#">${theList.category_name}</a>
     `;
         ulList.appendChild(li)
         // console.log(theList.category_id)
@@ -21,7 +22,7 @@ const displaycategoriList = allList => {
 
 
 
-const loadCardData = (category_id) => {
+const loadData = (category_id) => {
     console.log(category_id)
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
     fetch(url)
@@ -57,6 +58,30 @@ const displayCardData = allData => {
          </div>
     </div>
 </div>
+
+<div class="modal fade" id="see-more" tabindex="-1" aria-labelledby="see-moreLabel" aria-hidden="true">
+<div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="see-moreLabel">${theData.title}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+           ${theData.details}
+        </div>
+        <div class="d-flex">
+            <img style="height:50px ; border-radius: 50%;" src="${theData.author.img}" alt="">
+            <div>
+                 <p class="fs-5">${theData.author.name}</p>
+                <p class="fs-6">${theData.author.published_date}</p>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
+</div>
     `
         cardCont.appendChild(theDiv)
     });
@@ -65,7 +90,7 @@ const displayCardData = allData => {
 const loadNewsData = (news_id) => {
     // console.log('get detail', news_id);
     const url = `https://openapi.programming-hero.com/api/news/${news_id}`
-    // console.log(url);
+    console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => console.log(data))
